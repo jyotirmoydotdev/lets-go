24994e5617cc488951dfe3132f7b8024e624ed03
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2025-02-12 11:58:19 +0530

    Leveled logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,22 +4,32 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main()  {
-  addr := flag.String("addr", ":4000", "Http network address")
-  flag.Parse()
-  mux := http.NewServeMux()
+func main() {
+	addr := flag.String("addr", ":4000", "Http network address")
+	flag.Parse()
 
-  fileServer := http.FileServer(http.Dir("./ui/static")) 
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-  mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux := http.NewServeMux()
 
-  mux.HandleFunc("/", home)
-  mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
+	fileServer := http.FileServer(http.Dir("./ui/static"))
 
-  log.Printf("Starting server on %s", *addr)
-  err := http.ListenAndServe(*addr, mux)
-  log.Fatal(err)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
