2450fb2ef821f742c4502d2e85e9228ec90aa18a
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2025-02-17 20:06:56 +0530

    Transactions and other details

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -19,7 +19,9 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-	stmt := `INSERT into snippets (title, content, created, expires) VALUES(?,?,UTC_TIMESTAMP(),DATE_ADD(UTC_TIMESTAMP(), INTERVAL ?  DAY))`
+	stmt := `INSERT into snippets (title, content, created, expires) 
+  VALUES(?,?,UTC_TIMESTAMP(),DATE_ADD(UTC_TIMESTAMP(), INTERVAL ?  DAY))`
+
 	result, err := m.DB.Exec(stmt, title, content, expires)
 	if err != nil {
 		return 0, nil
