dc80e80b499597aca84b33c3753da66ec47ea7fe
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2025-02-17 15:54:02 +0530

    Single-record SQL queries

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/jyotirmoydotdev/lets-go/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -49,8 +52,17 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Printf("%d - /snippet/view?id=%d - All Ok", http.StatusOK, id)
-	fmt.Fprintf(w, "display a specific snippet of ID: %d....", id)
+	snippet, err := app.snippets.Get(id)
+
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -61,15 +73,15 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-  title := "0 snail"
-  content := "0 snail\nClimb Mount Fuji, \nBut slowly, slowly!\n\n- Kobayashi Issa"
-  expires := 7
+	title := "0 snail"
+	content := "0 snail\nClimb Mount Fuji, \nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
 
-  id, err := app.snippets.Insert(title, content, expires)
-  if err != nil {
-    app.serverError(w, err)
-    return
-  }
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-  http.Redirect(w, r , fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -1,40 +1,53 @@
 package models
 
 import (
-  "database/sql"
-  "time"
+	"database/sql"
+	"errors"
+	"time"
 )
 
 type Snippet struct {
-  ID int 
-  Title string 
-  content string
-  Created time.Time
-  Expires time.Time
+	ID      int
+	Title   string
+	content string
+	Created time.Time
+	Expires time.Time
 }
 
 type SnippetModel struct {
-  DB *sql.DB
+	DB *sql.DB
 }
 
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-  stmt:= `INSERT into snippets (title, content, created, expires) VALUES(?,?,UTC_TIMESTAMP(),DATE_ADD(UTC_TIMESTAMP(), INTERVAL ?  DAY))`
-  result, err := m.DB.Exec(stmt, title, content, expires)
-  if err != nil {
-    return 0, nil
-  }
-  
-  id , err := result.LastInsertId()
-  if err != nil {
-  return 0, nil 
-  }
-  return int(id), err
+	stmt := `INSERT into snippets (title, content, created, expires) VALUES(?,?,UTC_TIMESTAMP(),DATE_ADD(UTC_TIMESTAMP(), INTERVAL ?  DAY))`
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, nil
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, nil
+	}
+	return int(id), err
 }
 
-func (m *SnippetModel) Get(id int)(*Snippet, error){
-  return nil, nil 
+func (m *SnippetModel) Get(id int) (*Snippet, error) {
+	s := &Snippet{}
+
+	err := m.DB.QueryRow(`SELECT id, title, content, created, expires FROM snippets
+  WHERE expires > UTC_TIMESTAMP() AND id = ?`, id).Scan(&s.ID, &s.Title, &s.content, &s.Created, &s.Expires)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	return s, nil
 }
 
-func (m *SnippetModel) Latest() ([]*Snippet, error)  {
-  return nil, nil 
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	return nil, nil
 }
