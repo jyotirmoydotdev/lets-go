8cf1a8b4c3f958a8c2cf4f99c2971619942daa38
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2025-02-26 23:59:52 +0530

    Validating form data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
+	"filippo.io/edwards25519/field"
 	"github.com/julienschmidt/httprouter"
 	"github.com/jyotirmoydotdev/lets-go/internal/models"
 )
@@ -25,7 +28,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-  params := httprouter.ParamsFromContext(r.Context())
+	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
@@ -50,30 +53,50 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-  data := app.newTemplateData(r)
-  app.render(w, http.StatusOK, "create.tmpl", data)
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-  err := r.ParseForm()
-  if err != nil {
-    app.clientError(w, http.StatusBadRequest)
-    return
-  }
-  title := r.PostForm.Get("title")
-  content := r.PostForm.Get("content")
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
 
-  expires, err := strconv.Atoi(r.PostForm.Get("expires"))
-  if err != nil {
-    app.clientError(w, http.StatusBadRequest)
-    return
-  }
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
-  id, err := app.snippets.Insert(title, content, expires)
-  if err != nil {
-    app.serverError(w,err)
-    return 
-  }
+	fieldErros := make(map[string]string)
+	if strings.TrimSpace(title) == "" {
+		fieldErros["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErros["title"] = "This field cannot be more than 100 characters long"
+	}
 
-  http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d",id), http.StatusSeeOther)
+	if strings.TrimSpace(content) == "" {
+		fieldErros["content"] = "This field cannot be blank"
+	}
+
+	if expires != 1 && expires != 7 && expires != 365 {
+		fieldErros["errors"] = "This field must equal 1, 7 or 365"
+	}
+
+	if len(fieldErros) > 0 {
+		fmt.Fprint(w, fieldErros)
+		return
+	}
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
