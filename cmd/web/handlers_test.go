a89b455c9b503102f008092de1c9794d7e9cbfb0
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2025-03-10 17:25:57 +0530

    End-to-end testing

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,31 +1,19 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/jyotirmoydotdev/lets-go/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	app := newTestApplication(t)
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	ping(rr, r)
-	rs := rr.Result()
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK")
+	code, _, body := ts.get(t, "/ping")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
