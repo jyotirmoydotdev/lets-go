adf507c8f404a39eb34d6a16a82ad8c9205a7e31
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2025-03-11 19:31:42 +0530

    Mocking dependencies

--- internal/models/users.go
+++ internal/models/users.go
@@ -10,6 +10,12 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+type UserModelInterface interface {
+	Insert(name, email, password string) error
+	Authenticate(email, password string) (int, error)
+	Exists(id int) (bool, error)
+}
+
 type User struct {
 	ID             int
 	Name           string
@@ -69,8 +75,8 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
-  var exists bool
-  stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
-  err := m.DB.QueryRow(stmt, id).Scan(&exists)
-  return exists, err
+	var exists bool
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+	return exists, err
 }
